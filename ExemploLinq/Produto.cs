199493e1b55e7b56956f94e8f2759c61ba9039dc
jeanm <jeanm@JEANMICHAEL>
2022-04-21 20:48:17 -0300

    Adicionar arquivos de projeto.

---
+++ ExemploLinq/Produto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExemploLinq
+{
+    public class Produto
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public bool Status { get; set; }
+        public decimal Valor { get; set; }
+        public int CategoriaId { get; set; }
+    }
+}
